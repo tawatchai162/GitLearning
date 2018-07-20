fc5074d2b065a7c13e24a993a705947bb29ca262
tawatchai <tawatchaik@hotmail.com>
2018-07-20 17:08:43 +0700

    modified 0.0.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String version = "0.0.1";
+        String version = "0.0.2";
         System.out.println("Hello World! " + version);
     }
 }
