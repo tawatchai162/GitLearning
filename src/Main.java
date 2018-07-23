d9202e247ff460fa622a67753292bdb05e2d5bd8
tawatchai <tawatchaik@hotmail.com>
2018-07-23 16:19:27 +0700

    modified 0.0.5-develop

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String version = "0.0.4";
+        String version = "0.0.5-develop";
         System.out.println("Hello World! " + version);
     }
 }
