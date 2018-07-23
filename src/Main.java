54760e63afdb949ff5bb0bced3bd9c6590f121e8
tawatchai <tawatchaik@hotmail.com>
2018-07-23 17:11:38 +0700

    modified 0.0.8-feature1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        String version = "0.0.6-develop";
+        String version = "0.0.8-develop";
         String feature = "1";
         System.out.println("Hello World! " + version + " and feature! " + feature);
     }
