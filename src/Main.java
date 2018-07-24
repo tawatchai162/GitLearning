c511e083b73b86b1b18e9524203b81d9e64d526d
tawatchai <tawatchaik@hotmail.com>
2018-07-24 21:55:54 +0700

    modify feature11

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String version = "0.0.12-develop";
-        String feature = "3";
+        String feature = "4";
         System.out.println("Hello World! " + version + " and feature! " + feature);
     }
 }
