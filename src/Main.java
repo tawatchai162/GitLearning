9b0cb1c2a48bef31ed78a89839a428ce6777ca93
tawatchai <tawatchaik@hotmail.com>
2018-07-24 21:15:14 +0700

    version "0.0.12-develop

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String version = "0.0.9-develop";
-        String feature = "1-2";
+        String version = "0.0.12-develop";
+        String feature = "1";
         System.out.println("Hello World! " + version + " and feature! " + feature);
     }
 }
