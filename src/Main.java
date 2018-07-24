473e1a1a188f54c263d696cd4cba87044ff91b1d
tawatchai <tawatchaik@hotmail.com>
2018-07-24 20:59:00 +0700

    Merge branch 'develop' of https://github.com/tawatchai162/GitLearning into develop
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String version = "0.0.9-develop";
-        String feature = "1-1";
+        String feature = "1-2";
         System.out.println("Hello World! " + version + " and feature! " + feature);
     }
 }
