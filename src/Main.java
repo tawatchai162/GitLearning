2c762cef3f6363477545a893d8f3bd8bb8468597
tawatchai <tawatchaik@hotmail.com>
2018-07-25 13:59:42 +0700

    modify feature change to 7

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String version = "0.0.12-develop";
-        String feature = "6";
+        String feature = "7";
         System.out.println("Hello World! " + version + " and feature! " + feature);
     }
 }
