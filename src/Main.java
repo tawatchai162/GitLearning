f290e90848c489c09db4e6ac503c2d7fd1a1b5c8
tawatchai <tawatchaik@hotmail.com>
2018-07-25 18:01:27 +0700

    commit release 0.01

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         String version = "0.0.12-develop";
         String feature = "8";
-        System.out.println("Hello World! " + version + " and feature! " + feature);
+        String release = "0.0.1";
+
+        System.out.println("Hello World! " + version + " and feature! " + feature + " release ! " + release);
     }
 }
